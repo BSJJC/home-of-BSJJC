d18c1d9a18aa4c8b3f4c9e5fca4b8828b1bc9d42
BSJJC <805086021@qq.com>
2022-12-30 16:23:30 +0800

    Add: tailwindcss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <div>home products</div>
+    <h1 class="text-3xl font-bold underline bg-red-400">
+      Hello world!
+    </h1>
+    <div>
+    </div>
   </div>
 
   <slot />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 export default defineNuxtConfig({
-  css: ['ant-design-vue/dist/antd.css'],
+  css: ['ant-design-vue/dist/antd.css', '/assets/css/main.css'],
 
   plugins: ["@/plugins/antd"],
 
   vite: {
     define: {
       "process.env.DEBUG": false,
+    },
+  },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
   },
 });

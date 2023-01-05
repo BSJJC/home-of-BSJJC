7653c7aedd7440f17473a3dcf1044430e2fa90d2
BSJJC <805086021@qq.com>
2023-01-05 13:54:51 +0800

    End: Element UI icon installed

--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -1,7 +1,12 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/display.css'
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(ElementPlus)
+  nuxtApp.vueApp.use(ElementPlus);
+
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    nuxtApp.vueApp.component(key, component);
+  }
 })
